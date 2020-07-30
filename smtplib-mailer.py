8f54c53d10fe3aae2dab4a8e9a1f9010111263d3
Thom Huynh <thomhuynh@Thoms-MacBook-Air.local>
2020-07-30 23:06:49 +0200

    added white space deletion from front and back for csv cells

--- smtplib-mailer.py
+++ smtplib-mailer.py
@@ -13,12 +13,12 @@ with open('names.csv', 'r') as csv_file:
 #context manager for connection handling
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
   smtp.login(config['login'], config['password'])
-  
+
   for tuple in msg_tuples:
     msg = EmailMessage()
     msg['From'] = config['login']
-    msg['To'] = tuple[0]
-    msg['Subject'] = tuple[3]
-    msg.set_content(tuple[4])
+    msg['To'] = tuple[0].strip()
+    msg['Subject'] = tuple[3].strip()
+    msg.set_content(tuple[4].strip())
     smtp.send_message(msg)
     print(f"Sent to: {tuple[0]}; {tuple[1]} {tuple[2]}")
