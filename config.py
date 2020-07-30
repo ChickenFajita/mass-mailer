d1b84ec4640b15d3cffb79e1d30ab8073fcb8263
Thom Huynh <thomhuynh@Thoms-MacBook-Air.local>
2020-07-30 23:02:00 +0200

    implemented support for extended csv format to allow individual subjects and bodies for each recipient

--- config.py
+++ config.py
@@ -1,6 +1,4 @@
 config = {
     'login': 'thom.huynh06@gmail.com',
     'password': 'lcbuloymhvkgnqua',
-    'subject': 'Want food?',
-    'body' : 'Hello, lets have dinner tonight!',
 }
--- smtplib-mailer.py
+++ smtplib-mailer.py
@@ -1,25 +1,25 @@
 import smtplib
 import csv
-import os
 from config import config
 from email.message import EmailMessage
 
-contacts = []
+msg_tuples = []
 #context manager
 with open('names.csv', 'r') as csv_file:
-  csv_reader = csv.reader(csv_file)
+  csv_reader = csv.reader(csv_file, delimiter=';')
   for line in csv_reader:
-    contacts.append(line[0])
-
-msg = EmailMessage()
-msg['Subject'] = config['subject']
-msg['From'] = config['login']
-msg['To'] = ', '.join(contacts)
-msg.set_content(config['body'])
+    msg_tuples.append(line)
 
 #context manager for connection handling
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
   
   smtp.login(config['login'], config['password'])
-  smtp.send_message(msg)
-  print("Emails sent!")
+
+  for tuple in msg_tuples:
+    msg = EmailMessage()
+    msg['From'] = config['login']
+    msg['To'] = tuple[0]
+    msg['Subject'] = tuple[3]
+    msg.set_content(tuple[4])
+    smtp.send_message(msg)
+    print(f"Sent to: {tuple[0]}; {tuple[1]} {tuple[2]}")
